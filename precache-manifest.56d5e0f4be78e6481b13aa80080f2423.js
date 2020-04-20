a54d9d5553bcf9962a9daff79a8c211c8ba8806a
Simon Dunkley <simon.j.dunkley@gmail.com>
2020-04-20 14:58:38 +1000

    Updates

---
+++ precache-manifest.56d5e0f4be78e6481b13aa80080f2423.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea5051ee1f9cd7548281b55f750f5c3",
+    "url": "/SimonDunk/app-dev/index.html"
+  },
+  {
+    "revision": "af64316343e4b7dff9e0",
+    "url": "/SimonDunk/app-dev/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c2fd1259413a1583c19d",
+    "url": "/SimonDunk/app-dev/static/js/2.65688262.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimonDunk/app-dev/static/js/2.65688262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af64316343e4b7dff9e0",
+    "url": "/SimonDunk/app-dev/static/js/main.82700ae1.chunk.js"
+  },
+  {
+    "revision": "33a128a071e230484962",
+    "url": "/SimonDunk/app-dev/static/js/runtime-main.8d727ea9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SimonDunk/app-dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
